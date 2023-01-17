637b86b737d9d66a171715f2d5bb083c1c8d4bea
marcoaufiero <marcoaufiero96@gmail.com>
2023-01-17 13:02:42 +0100

    table trains created

---
+++ database/migrations/2023_01_17_115112_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure', 100);
+            $table->string('arrival', 100);
+            $table->string('departure_time', 100);
+            $table->string('arrival_time', 100);
+            $table->string('ticket_price', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
